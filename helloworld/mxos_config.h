90f9fb8f90c6b1c389c94aae38fb8a86104583d2
Snow Yang <157690863@qq.com>
2019-10-30 10:17:49 +0800

    规范代码格式，删除 os demo

--- helloworld/mxos_config.h
+++ helloworld/mxos_config.h
@@ -1,7 +1,7 @@
 /**
 ******************************************************************************
 * @file    mxos_config.h
-* @author  William Xu
+* @author  Snow Yang
 * @version V1.0.0
 * @date    08-Aug-2018
 * @brief   This file provide application options diff to default.
